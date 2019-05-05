4439138453e68e327ccc3c79000744a02cc7c7f2
nascVi <vaiiho.nascimbene@gmail.com>
2019-05-05 19:08:44 +0200

    hooks ready arch files

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import FileUpload from './components/FileUpload'
+import './App.css'
 
-const App = () => (
-    <div className="App">
+const App = () => <div className='container mt-4'>
+  <h4 className='display-4 text-center mb-4'>
+    <i className='fab fa-react'/> R-Upload Files
+  </h4>
 
-    </div>
-)
+  <FileUpload />
+</div>
 
-export default App;
+export default App
