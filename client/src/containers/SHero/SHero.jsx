e599f48788ab87ca2fb027172d4f8516c6ba8be0
nascVi <vaiiho.nascimbene@gmail.com>
2019-05-17 20:55:53 +0200

    bg err fixed

--- client/src/containers/SHero/SHero.jsx
+++ client/src/containers/SHero/SHero.jsx
@@ -1,18 +1,18 @@
-import React, {/* useState */} from 'react'
+import React, { /* useState */ } from 'react'
 import FadeInLogo from '../../components/HeroH/FadeinLogo'
-/*import NavStrategButtons from '../../components/HeroH/NavStrategButtons'
-import hBackground from '../../components/SVGs/shero/hBackground.svg'*/
+import hBackground from '../../components/SVGs/shero/hBackground.svg'
 
-const Hero = () =>
-// Afficher logo avec effet de fondu à ouverture
+const Hero = () => 
+    // Afficher logo avec effet de fondu à ouverture
     <div className="hero">
-     <h1>I am the hero here!</h1>
-     <hBackground />
-     <FadeInLogo />
-     {/* Following section is using the onLoad 
-method, displaying the function ready strategButtons
-     the adrian way
-     <NavStrategButtons />*/}
-    </div>
+    <h1>I am the hero here!</h1> 
+    <hBackground />
+    <FadeInLogo /> 
+    {/* Following section is using the onLoad 
+    method, displaying the function ready strategButtons
+    the adrian way
+    <NavStrategButtons />*/} 
+     </div>
+
 
 export default Hero
