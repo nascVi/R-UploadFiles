fc4a7bf0e6ca9a08dfbd3b5abcb680006c1507e7
Nascimbene <vaiiho.nascimbene@gmail.com>
2019-05-24 06:16:41 +0200

    css after-shok

---
+++ client/src/components/HeroH/ExampleReactTransitionGroup.jsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+import { CSSTransitionGroup } from 'react-transition-group';
+
+const ExampleReactTransitionGroup = () => {
+    return (
+        
+    );
+};
+
+export default ExempleReactTransitionGroup;
--- client/src/components/HeroH/ExempleReactTransitionGroup.jsx
+++
@@ -1,1 +0,0 @@
-ExempleReactTransitionGroup.jsx
