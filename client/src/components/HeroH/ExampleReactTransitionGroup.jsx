97f70567229b8b0e7cb955ca88150ecf7c6a39ea
Nascimbene <vaiiho.nascimbene@gmail.com>
2019-06-02 08:25:24 +0200

    pkgs audits

--- client/src/components/HeroH/ExampleReactTransitionGroup.jsx
+++ client/src/components/HeroH/ExampleReactTransitionGroup.jsx
@@ -1,9 +1,34 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { CSSTransitionGroup } from 'react-transition-group';
+import random from "random-name";
+import Button from "./Button";
+import Item from ".item";
+import 'style.css';
 
 const ExampleReactTransitionGroup = () => {
+
+	const [state, setState] = useState();
+	
+	useEffect(() => {
+		
+	}, [])
+
+	const handleAdd = () => {
+		let newItems = state.items;
+		new.Items.push(random());
+		setState({ items: newItems });
+	}
+
+	const handleRemove = (i) => {
+		let newItems = state.items.slice();
+		newItems.splce(i, 1);
+		setState({items: newItems});
+	}
+
     return (
-        
+		<Fragment>
+		This is ReactCssTransitionGroup happening
+		</Fragment>
     );
 };
 
